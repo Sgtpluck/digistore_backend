f210ba983ce7e11fb64e47bd420832edc45dd481
Davida Marion <davidamarion@gmail.com>
2014-03-19 09:33:43 -0700

    working on items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,4 +6,17 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      render :show
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:quantity, :product, :cart, :current_price)
+  end
 end
